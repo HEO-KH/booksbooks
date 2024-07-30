e18fadcdaf1cef993ca14f163b72157656759a5f
HEO-KH <gjrkdgus0316@naver.com>
2024-07-30 14:47:06 +0900

    first KRbook commit
    책 정보에 대한 DAO, DTO, Servlet 기본 셋팅

---
+++ krbook/Books_KrbookDTO.java
@@ -0,0 +1,72 @@
+package com.krbook;
+
+public class Books_KrbookDTO {
+	
+	private String ISBN;
+	private String subject;
+	private String category1;
+	private String category2;
+	private String author;
+	private String bookdate;
+	private String page;
+	private String booksize;
+	private String price;
+	
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getCategory1() {
+		return category1;
+	}
+	public void setCategory1(String category1) {
+		this.category1 = category1;
+	}
+	public String getCategory2() {
+		return category2;
+	}
+	public void setCategory2(String category2) {
+		this.category2 = category2;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBookdate() {
+		return bookdate;
+	}
+	public void setBookdate(String bookdate) {
+		this.bookdate = bookdate;
+	}
+	public String getPage() {
+		return page;
+	}
+	public void setPage(String page) {
+		this.page = page;
+	}
+	public String getBooksize() {
+		return booksize;
+	}
+	public void setBooksize(String booksize) {
+		this.booksize = booksize;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+	
+}
